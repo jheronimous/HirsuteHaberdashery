b6482dc89a0e1aad7966900da933ea9572ba686c
Joe Robb <joerobb@Joes-MacBook-Air.local>
2018-04-26 00:45:22 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/hirsutehaberdashery/HirsuteHaberdasheyInfo.java
@@ -0,0 +1,13 @@
+package com.example.android.hirsutehaberdashery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HirsuteHaberdasheyInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hirsute_haberdashey_info);
+    }
+}
